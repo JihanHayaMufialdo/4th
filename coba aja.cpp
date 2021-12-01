0c109c54a0c2143213563d963d343619a441c295
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-12-01 21:37:11 +0700

    Update coba aja.cpp

--- coba aja.cpp
+++ coba aja.cpp
@@ -2,25 +2,10 @@
 using namespace std;
 int main(){
 	
-	//kode buku
+	for(int i=0; i<=5; i++){
+		mprintw(i,i,"J");
+	}
 	
-	int a[3] = {1212,1313,1414};
-	
-	cout << "masukkan kode buku : ";
-	cin >> a[3];
-	
-	if(a[3]==1212){
-		cout << "KATA by Rintik Sedu";
-	}
-	else if(a[3]==1313){
-		cout << "TENTANG KAMU by Tere Liye";
-	}
-	else if(a[3]==1414){
-		cout << "NEGERI PARA BEDEBAH by Tere Liye";
-	}
-	else{
-		cout << "!! BUKU TIDAK DITEMUKAN !!";
-	}
 	
 }		
 	
