7b4e3ca0e1fa6e622c30d8d235595d4e5446d66d
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-12-01 21:45:50 +0700

    Update coba aja.cpp

--- coba aja.cpp
+++ coba aja.cpp
@@ -3,23 +3,23 @@ int main(){
 
 initscr();
 	
-	for(int i=0; i<=5; i++){
+	for(int i=0; i<=4; i++){
 		mvprintw(i,i,"J");
 	}
 	
-	for(int i=1; i<=6; i++){
+	for(int i=1; i<=5; i++){
 		mvprintw(i,i+2,"I");
 	}
 	
-	for(int i=2; i<=7; i++){
+	for(int i=2; i<=6; i++){
 		mvprintw(i,i+4,"H");
 	}
 	
-	for(int i=3; i<=8; i++){
+	for(int i=3; i<=7; i++){
 		mvprintw(i,i+6,"A");
 	}
 	
-	for(int i=4; i<=9; i++){
+	for(int i=4; i<=8; i++){
 		mvprintw(i,i+8,"N");
 	}
 	
